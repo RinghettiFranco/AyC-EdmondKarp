faed31beca594f6534525ce33bc49b1c20f7958e
RinghettiFranco <54511049+RinghettiFranco@users.noreply.github.com>
2021-06-28 16:55:30 -0300

    asd

--- src/main/kotlin/Actividad13-EK-Ringhetti-Talmon.kt
+++ src/main/kotlin/Actividad13-EK-Ringhetti-Talmon.kt
@@ -1,31 +1,13 @@
+import java.util.*
 
 fun main(args: Array<String>) {
-    println("Hello World!")
-    val n = Integer.valueOf(readLine())
-    val m = Integer.valueOf(readLine())
-    val s = Integer.valueOf(readLine())
-    val t = Integer.valueOf(readLine())
+    val scanner = Scanner(System.`in`)
+    val n = scanner.nextInt()
+    val m = scanner.nextInt()
+    val s = scanner.nextInt()
+    val t = scanner.nextInt()
     val ek: EdmondKarp = EdmondKarp(n,m,s,t)
     ek.inicializarGrafo()
-}
-
-class Arco(
-    protected val o: Int,
-    protected val d: Int,
-    protected var residual: Arco? = null,
-    protected var flujo: Int = 0,
-    protected val capacidad: Int
-){
-    fun esResidual(): Boolean = capacidad == 0
-
-    fun capacidadDisponible(): Int = capacidad - flujo
-
-    fun aumentar(aumento: Int) {
-        flujo = flujo + aumento
-        residual?.let{
-            it.flujo = it.flujo - aumento
-        }
-    }
 }
 
 class EdmondKarp(
@@ -34,21 +16,50 @@ class EdmondKarp(
     protected var s: Int,
     protected var t: Int,
 ) {
+    class Arco(
+        val o: Int,
+        val d: Int,
+        var residual: Arco? = null,
+        var flujo: Int = 0,
+        val capacidad: Int
+    ){
+        fun esResidual(): Boolean = capacidad == 0
+
+        fun capacidadDisponible(): Int = capacidad - flujo
+
+        fun aumentar(aumento: Int) {
+            flujo = flujo + aumento
+            residual?.let{
+                it.flujo = it.flujo - aumento
+            }
+        }
+    }
+
+    private val scanner = Scanner(System.`in`)
     private var token: Int = 1
     private val visitados: Array<Int> = arrayOf(n+1)
-    //solved
-    val grafo: Array<MutableList<Arco>> = arrayOf()
+
+    private val grafo: Array<MutableList<Arco>> = arrayOf()
 
     fun inicializarGrafo() {
         for(i in (1..n)){
             grafo[i] = mutableListOf()
         }
         for(i in (1..m)){
-            val u = Integer.valueOf(readLine())
-            val v = Integer.valueOf(readLine())
-            val c = Integer.valueOf(readLine())
-            grafo[i].add(Arco(u,v,capacidad = c))
+            val u = scanner.nextInt()
+            val v = scanner.nextInt()
+            val c = scanner.nextInt()
+            añadirArco(u,v,c)
         }
+    }
+
+    fun añadirArco(o:Int, d:Int, c:Int) {
+        val arco = Arco(o,d,capacidad = c)
+        val residual = Arco(d,o,capacidad = 0)
+        arco.residual = residual
+        residual.residual = arco
+        grafo[o].add(arco)
+        grafo[d].add(residual)
     }
 
     fun visitar(n:Int) {
@@ -58,4 +69,8 @@ class EdmondKarp(
     fun visitado(n:Int): Boolean = visitados[n] == token
 
     fun desmarcarNodos() = token++
+
+    fun calcularFlujoMaximo():Int
+    
+    fun BFSEdmondKarp(): Int
 }
